9554b417fd68452695116019e3e2882358081086
sandeepkaurs <sandeep.singh@telus.com>
2023-02-01 09:35:54 -0800

    Commit exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram",last_name: "Virani",hourly_rate: 60)
+@store1.employees.create(first_name: "John",last_name: "Stamos",hourly_rate: 55)
+@store2.employees.create(first_name: "Bob",last_name: "Saget",hourly_rate: 50)
+@store2.employees.create(first_name: "Dave",last_name: "Coulier",hourly_rate: 45)
+@store1.employees.create(first_name: "Ashley",last_name: "Olsen",hourly_rate: 40)
+@store1.employees.create(first_name: "MaryKate",last_name: "Olsen",hourly_rate: 35)
